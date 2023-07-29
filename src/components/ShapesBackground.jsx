898452b94335e79a06f2034dd42083050c186a54
sabzdotpy <incrediblesabari02@gmail.com>
2023-07-29 16:32:01 +0530

    randomly gen n shapes

--- src/components/ShapesBackground.jsx
+++ src/components/ShapesBackground.jsx
@@ -1,5 +1,3 @@
-
-
 import Triangle from "../assets/Triangle.svg";
 import Circle from "../assets/Circle.svg";
 import Cube from "../assets/Cube.svg";
@@ -7,31 +5,51 @@ import Cube from "../assets/Cube.svg";
 import "../styles/ShapesBackground.scss";
 
 export default function ShapesBackground() {
-    
+    function rand(min, max) {
+        console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     return (
         <div className="bubblesBg">
-                <img src={Triangle} style={
-                    {
-                        position: "absolute",
-                        top: `${Math.random()*100}%`,
-                        left: `${Math.random()*100}%`,
-                    }
-                } />
-                <img src={Circle} style={
-                    {
-                        position: "absolute",
-                        top: `${Math.random()*100}%`,
-                        left: `${Math.random()*100}%`,
-                    }
-                } />
-                <img src={Cube} style={
-                    {
-                        position: "absolute",
-                        top: `${Math.random()*100}%`,
-                        left: `${Math.random()*100}%`,
-                    }
-                } />
-            </div>
-    )
+            {[...Array(rand(1,2))].map(() => {
+                return (
+                    <img
+                        src={Triangle}
+                        style={{
+                            position: "absolute",
+                            top: `${Math.random() * 100}%`,
+                            left: `${Math.random() * 100}%`,
+                        }}
+                    />
+                );
+            })}
+
+            {[...Array(rand(1,2))].map(() => {
+                return (
+                    <img
+                        src={Circle}
+                        style={{
+                            position: "absolute",
+                            top: `${Math.random() * 100}%`,
+                            left: `${Math.random() * 100}%`,
+                        }}
+                    />
+                );
+            })}
+
+            {[...Array(rand(1,1))].map(() => {
+                return (
+                    <img
+                        src={Cube}
+                        style={{
+                            position: "absolute",
+                            top: `${Math.random() * 100}%`,
+                            left: `${Math.random() * 100}%`,
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
 }
