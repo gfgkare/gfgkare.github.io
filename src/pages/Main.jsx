a0f5ec70ffe7441abe198a0448127f175bb0d19f
sabzdotpy <incrediblesabari02@gmail.com>
2023-07-29 19:56:06 +0530

    add ham

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import "../styles/Main.scss";
 import { useMisc } from "../contexts/MiscContext";
 
-import ScrollContainer from "../components/ScrollContainer";
+// import ScrollContainer from "../components/ScrollContainer";
 
 import gfgLogo from "../assets/GFG_KARE.svg";
 // import kluLogo from "../assets/klu.png";
-import { useNavigate } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx"
+// import { useNavigate } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Main() {
     const { theme, setTheme } = useMisc();
 
-    const navigate = useNavigate();
+    const [showNavBox, setShowNavBox] = useState(false);
+
+    // const navigate = useNavigate();
 
     return (
         <>
@@ -36,7 +39,9 @@ export default function Main() {
                             onClick={() => window.open("https://klu.ac.in")}
                         /> */}
                     </div>
-                    <span className="centerText hideOnMobile">GFG KARE STUDENT CHAPTER</span>
+                    <span className="centerText hideOnMobile">
+                        GFG KARE STUDENT CHAPTER
+                    </span>
 
                     <div className="rightMenu">
                         {/* <div className="dropDown">
@@ -49,10 +54,65 @@ export default function Main() {
                                 <div className="item">Home</div>
                             </div>
                         </div> */}
-                        
 
-                        <RxHamburgerMenu size={"25px"} />
+                        <div className="iconAndGrid">
+                            {/* <div className="comingSoon">Coming soon...</div> */}
 
+                            <div class="dropdown">
+                                <RxHamburgerMenu
+                                    size={"25px"}
+                                    id="hamburger"
+                                    onClick={() => {
+                                        setShowNavBox(
+                                            (showNavBox) => !showNavBox
+                                        );
+                                    }}
+                                />
+                                <div
+                                    className={`dropdown-content ${
+                                        showNavBox ? "show" : ""
+                                    }`}
+                                >
+                                    {/* <a href="#home">Home</a>
+                                    <a href="#about">About</a>
+                                    <a href="#contact">Contact</a> */}
+                                    <span>Coming soon...</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <ul
+								className={
+									"nav no-search" + (currentUser === "none" || !currentUser ? "" : " signed-in")
+								}
+								ref={navUl}
+							>
+									<li className="nav-item">
+										<div
+											className="apps-icon"
+											tabIndex={"0"}
+											onBlur={() => setAppsDrawerOpen(false)}
+										>
+											<MdOutlineApps
+												size={"30px"}
+												onClick={() => setAppsDrawerOpen(!appsDrawerOpen)}
+											/>
+											<CSSTransition
+												in={appsDrawerOpen}
+												timeout={300}
+												classNames="appsanim"
+												unmountOnExit
+											>
+												<ul className={"apps-dropdown"}>
+													<li>1</li>
+													<li>2</li>
+													<li>3</li>
+													<li>4</li>
+													<li>5</li>
+												</ul>
+											</CSSTransition>
+										</div>
+									</li>
+                                </ul> */}
 
                         {/* <input
                             type="checkbox"
@@ -70,7 +130,6 @@ export default function Main() {
                                 }
                             }}
                         /> */}
-
                     </div>
                 </div>
             </div>
