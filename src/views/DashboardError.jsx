3a13734d62701aff3abd7b637538a793d2d862bb
sabzdotpy <incrediblesabari02@gmail.com>
2023-12-29 15:22:06 +0530

    mobile algo24 error padding

--- src/views/DashboardError.jsx
+++ src/views/DashboardError.jsx
@@ -1,8 +1,10 @@
 import { useOutletContext, Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 
 
 export default function DashboardError() {
 
+    const { signinwithpopup } = useAuth();
     
     const [
         currentUser, circlePerc, isVisible, visualsRef, celebrate, animationDone, setAnimationDone, totalMarks, totalScoredMarks, 
@@ -11,8 +13,14 @@ export default function DashboardError() {
 
     return (
 
-        <div className="error">
+        <div className="error" style={ { padding: "0 1rem", display: "flex", flexDirection: "column", gap: "1rem" } }>
             {error}
+            <span>
+                <span style={ { maxWidth: "max-content", textDecoration: "underline", cursor: "pointer" } } onClick={() => signinwithpopup("google")}>
+                    Sign In{"  "}
+                </span> 
+                with another account.
+            </span>
         </div>
     )
 }
