40e571977a84438010130e2781f21c2c064deeab
sabzdotpy <incrediblesabari02@gmail.com>
2023-12-31 22:22:48 +0530

    pickle message, align fix

--- src/views/Pickle.jsx
+++ src/views/Pickle.jsx
@@ -4,8 +4,8 @@ export default function Pickle() {
 
 
     return (
-        <div className="pickle" style={ { fontWeight: "600", height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center"  } }  >
-            We are currently updating the database. Kindly wait for some time, we will let you know when the website is up.
+        <div className="pickle" style={ { padding: "0 .5rem", fontWeight: "600", height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center"  } }  >
+            We are currently updating the database. Kindly wait for some time, we will let you know when dashboard is up.
         </div>
     )
 }
