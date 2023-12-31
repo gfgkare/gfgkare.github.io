b5dead2e4e4a6e41c6f6d6de13451c550122d905
sabzdotpy <incrediblesabari02@gmail.com>
2023-12-31 22:25:47 +0530

    pickle padm

--- src/views/Pickle.jsx
+++ src/views/Pickle.jsx
@@ -4,7 +4,7 @@ export default function Pickle() {
 
 
     return (
-        <div className="pickle" style={ { padding: "0 .5rem", fontWeight: "600", height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center"  } }  >
+        <div className="pickle" style={ { fontWeight: "600", height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center"  } }  >
             We are currently updating the database. Kindly wait for some time, we will let you know when dashboard is up.
         </div>
     )
