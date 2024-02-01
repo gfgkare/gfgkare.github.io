0f7ab24cd1020573584786aa5c5c49bd2686dd6d
sabzdotpy <incrediblesabari02@gmail.com>
2024-02-02 00:00:45 +0530

    add codeathon details

--- src/views/NewEventRegister.jsx
+++ src/views/NewEventRegister.jsx
@@ -21,7 +21,7 @@ export default function NewEventRegister() {
 
                 <div className="text">
                     <div className="name gradientText">
-                        <div className="year">2024</div>
+                        <div className="year">Euphoria '24</div>
                         <div className="event">Codeathon</div>
                     </div>
                     <div className="shortAbout">
@@ -33,6 +33,41 @@ export default function NewEventRegister() {
                 </div>
             </section>
 
+            <section className="about">
+                Codeathon, a challenging algorithmic event under EUPHORIA 2K24 held by Kalasalingam Academy of Research and Education.
+                This competetion challenges and invites the participants all over the country to showcase their excellent problem-solving skills
+                in the area Algorithmic Analysis and Competetive Programming.
+            </section>
+
+            <section className="rounds">
+                <div className="round">
+                    <div className="title">
+                        Round 1
+                    </div>
+                    <div className="info">
+                        <span>26th Match 2024 - 10AM</span>
+                        <span>80 Participants</span>
+                        <span>30 MCQs</span>
+                    </div>
+                </div>
+                <div className="round">
+                    <div className="title">
+                        Round 2
+                    </div>
+                    <div className="info">
+                        <span>26th Match 2024 - 4PM</span>
+                        <span>40 Participants</span>
+                        <span>3 Coding Questions</span>
+                    </div>
+                </div>
+            </section>
+            
+            <section className="ctaReminder">
+                <button className="cta">
+                    Register Now!
+                </button>
+            </section>
+            
 			<section className="sponsors">
 				<div className="klu">KLU</div>
 				<div className="gfgkare">GFG KARE</div>
