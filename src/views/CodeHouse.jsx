f2fc8e2c8d8e172640e6f9019b9ae9a47ad9f0d9
sabzdotpy <incrediblesabari02@gmail.com>
2024-02-10 23:03:01 +0530

    add logged out/unauthorized email handle

--- src/views/CodeHouse.jsx
+++ src/views/CodeHouse.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 import useArray from "../hooks/useArray";
+import {useAuth} from "../contexts/AuthContext";
 
 import "../styles/CodeHouse.scss";
 
 export default function CodeHouse() {
 
+    const [userStatus, setUserStatus] = useState("out");
     const [pageToShow, setPageToShow] = useState("instructions");
     const [loadingPercentage, setLoadingPercentage] = useState(0);
+
+    const allowedEmails = ["incrediblesabari02@gmail.com", "9922008342@klu.ac.in"];
+
+    const { currentUser } = useAuth();
 
     const problemsList = useArray([
         {
@@ -49,6 +55,22 @@ export default function CodeHouse() {
         setTimeout(() => setPageToShow("code"), 6000);
     }
 
+
+    useEffect(() => {
+        console.log("currentUser", currentUser);
+        if (currentUser && currentUser !== "none") {
+            if (allowedEmails.includes(currentUser.email)) {
+                setUserStatus("approved");
+            }
+            else {
+                setUserStatus("rejected");
+            }
+        }
+        else {
+            setUserStatus("out");
+        }
+    })
+
     useEffect(() => {
         window.addEventListener('beforeunload', alertUser)
         return () => {
@@ -72,50 +94,88 @@ export default function CodeHouse() {
 
         <div className="codeHouse">
             {
-                (pageToShow === 'instructions' || pageToShow === 'loading') ? (
+                (userStatus === "approved") ? (
                     <>
                         {
-                            (pageToShow === "loading") ? (
-                                <div className="progressBar">
-                                    <div className="progressBar-thumb" style={{ width: `${loadingPercentage}%` }}></div>
-                                </div>
-                            ) : (
-                                <></>
-                            )
-                        }
-                    
-                        <div className="instructions">
-                            <div className="title">READ ME!</div>
-                            <div className="points">
-                                <ol>
-                                    <li>Do not exit from the page. You will die.</li>
-                                    <li>Do not try to switch tabs. You will die.</li>
-                                    <li>Do not try to copy and paste in the code editor. You will die.</li>
-                                    <li>Do not try to logout and login in your own or your friends' laptops. You will die.</li>
-                                    <li>Have fun! :)</li>
-                                </ol>
-                            </div>
-                            <button onClick={startRound} disabled={pageToShow === "loading"}>
+                        (pageToShow === 'instructions' || pageToShow === 'loading') ? (
+                            <>
                                 {
-                                    (pageToShow === "instructions") ? "Start" : "..."
+                                    (pageToShow === "loading") ? (
+                                        <div className="progressBar">
+                                            <div className="progressBar-thumb" style={{ width: `${loadingPercentage}%` }}></div>
+                                        </div>
+                                    ) : (
+                                        <></>
+                                    )
                                 }
-                            </button>
-                        </div>
+                            
+                                <div className="instructions">
+                                    <div className="title">READ ME!</div>
+                                    <div className="points">
+                                        <ol>
+                                            <li>Do not exit from the page. You will die.</li>
+                                            <li>Do not try to switch tabs. You will die.</li>
+                                            <li>Do not try to copy and paste in the code editor. You will die.</li>
+                                            <li>Do not try to logout and login in your own or your friends' laptops. You will die.</li>
+                                            <li>Have fun! :)</li>
+                                        </ol>
+                                    </div>
+                                    <button onClick={startRound} disabled={pageToShow === "loading"}>
+                                        {
+                                            (pageToShow === "instructions") ? "Start" : "..."
+                                        }
+                                    </button>
+                                </div>
 
+                            </>
+                        ) : (
+                            <></>
+                        )
+                    }
+                    
+                    {
+                        (pageToShow === "code") ? (
+                            <Outlet context={ { problemsList, problemsCode, finishRound } } />
+                        ) : (
+                            <></>
+                        )
+                    }
                     </>
                 ) : (
                     <></>
                 )
             }
-            
             {
-                (pageToShow === "code") ? (
-                    <Outlet context={ { problemsList, problemsCode, finishRound } } />
+                (userStatus === "rejected") ? (
+                    <div className="rejected">
+                        <div className="title">Sorry!</div>
+                        <div className="points">
+                            <ul>
+                                <li>Your email ({currentUser.email}) is not allowed to participate in this event.</li>
+                                <li>Kindly contact the event organizers for more details.</li>
+                                <li>
+                                    <Link to={"/profile"} state={{from: location.pathname}} >Sign in with another email?</Link>
+                                    {/* im tired. 'a' not getting margin even if i mention in css, wrapping in li to inherit li properties to get some spacing. */}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 ) : (
                     <></>
                 )
             }
-
+            {
+                (userStatus === "out") ? (
+                    <div className="out">
+                        <div className="title">You are not logged in.</div>
+                        <div>
+                            You are not logged in. Please <Link to={"/profile"} state={{from: location.pathname}} >Sign in</Link> to continue.
+                        </div>
+                    </div>
+                ) : (
+                    <></>
+                )
+            }
         </div>
 
     )
