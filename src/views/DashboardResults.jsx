90f9fc24aa719b875a3b2bbcbdf3c6e7be9e54be
sabzdotpy <incrediblesabari02@gmail.com>
2024-02-11 23:13:22 +0530

    rm unneces splitter

--- src/views/DashboardResults.jsx
+++ src/views/DashboardResults.jsx
@@ -64,7 +64,6 @@ export default function DashboardResults(props) {
             <div className="resultBigTitle">Results</div>
 
             {/* <div className="title">Round 3 - QnA</div> */}
-            
             {/* <div className="round3">
                 <div className="teamBoxesContainer">
 
@@ -93,9 +92,7 @@ export default function DashboardResults(props) {
                     }
                 </div>
             </div> */}
-                
-
-            <div className="splitter"></div>
+            {/* <div className="splitter"></div> */}
 
             <div className="title">Round 2 - Seminar</div>
             <div className="round2Best">
