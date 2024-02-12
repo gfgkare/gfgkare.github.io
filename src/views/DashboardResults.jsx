a1924f3594f08684ed956505a5a87b69228fc3c4
Sabari S <82570809+sabzdotpy@users.noreply.github.com>
2024-02-12 16:38:52 +0530

    Update DashboardResults.jsx

--- src/views/DashboardResults.jsx
+++ src/views/DashboardResults.jsx
@@ -63,8 +63,8 @@ export default function DashboardResults(props) {
         <div className="dashboardResults">
             <div className="resultBigTitle">Results</div>
 
-            {/* <div className="title">Round 3 - QnA</div> */}
-            {/* <div className="round3">
+            <div className="title">Round 3 - QnA</div>
+            <div className="round3">
                 <div className="teamBoxesContainer">
 
                     {
@@ -89,10 +89,10 @@ export default function DashboardResults(props) {
                                 </div>
                             </div>
                         ))
-                    }
+                      }
                 </div>
-            </div> */}
-            {/* <div className="splitter"></div> */}
+            </div>
+            <div className="splitter"></div>
 
             <div className="title">Round 2 - Seminar</div>
             <div className="round2Best">
