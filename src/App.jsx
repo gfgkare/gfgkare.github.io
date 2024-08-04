55ea509d7de024c838c69cc6f4aa387d61469a16
sabzdotpy <incrediblesabari02@gmail.com>
2024-08-04 10:36:18 +0530

    rm dynamic events link

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ export default function App() {
                                 })
                             }
 
-                            <Route path="/events/:eventname" element={<DynamicEventPage />}></Route>
+                            {/* <Route path="/events/:eventname" element={<DynamicEventPage />}></Route> */}
     
                             <Route path="/meta" element={ <Meta /> }></Route>
                         </Route>
