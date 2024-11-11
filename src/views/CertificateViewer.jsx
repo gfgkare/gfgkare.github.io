180244833a662f6b0213f66929236a73f21d0bdf
sabzdotpy <incrediblesabari02@gmail.com>
2024-11-11 15:00:02 +0530

    Certificate verify message

--- src/views/CertificateViewer.jsx
+++ src/views/CertificateViewer.jsx
@@ -1,29 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function CertificateViewer() {
 
     const params = useParams();
+    const [errored, setErrored] = useState(null);
 
     useEffect(() => {
         console.log(params);
         
     }, []);
 
-    try {
-        const image = `/certificates/${params.certificateID}.jpg`;
-        return (
-            <div className="certificateViewer">
-                <img src={image} alt={params.certificateID} style={{ width: '100%', objectFit: "contain" }} />
-                <span style={{ textAlign: "center", margin: "0 auto" }}>
-                    This certificate is valid and is provided to {params.certificateID} for his/her active participation in Prajnotsavah by GFG KARE.
-                </span>
-            </div>
+    return (
+        (!errored) ?
+        <div className="certificateViewer">
+            <img src={`/certificates/${params.certificateID}.jpg`} alt={params.certificateID} style={{ width: '100%', objectFit: "contain" }} onError={() => {
+                setErrored("Not Found.")
+            }} />
+            <p style={{ margin: "1rem", fontWeight: 600 }}>
+                This certificate is valid and is provided to {params.certificateID} for his/her active participation in Prajnotsavah by GFG KARE.
+            </p>
+        </div>  : (
+            <p style={{ margin: "5rem", textAlign: "center", fontWeight: 600 }}>
+                The certificate you requested is not found in our records.
+            </p>
         )
-        
-    } catch (error) {
-        console.log(error);
-        
-        return <p>Certificate not found</p>;
-    }
+    )    
 }
